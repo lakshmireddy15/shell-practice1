a1c2d7f05735f224a97f064c4f46c6730bad2f0e
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-31 22:24:47 +0530

    1st commit

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,5 +3,5 @@
 MOVIES=('HIT','Court','rebel')
 
 echo "first movie: ${MOVIES[0]}"
-echo "second movie: ${MOVIES[1]}"
-echo "last movie: ${MOVIES[2]}"
+echo "second movie: ${MOVIES[3]}"
+echo "last movie: ${MOVIES[4]}"
