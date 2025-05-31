169f7321dfc4c30c6f46aab058c4e2fc9d80c21d
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-31 18:28:57 +0530

    1st commit

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=ram
+PERSON2=sita
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2 : Hi $PERSON1, I'm doing well. How about you?"
+echo "$PERSON1 : I'm good too $PERSON2. Long time no see!"
+echo "$PERSON2 : Yes, it's been a while $PERSON1 . Let's catch up soon."
