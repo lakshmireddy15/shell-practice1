e63304ae20fa289a8395487ba64935a1458e910e
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-31 18:21:05 +0530

    1st commit

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ram : Hi Sita, how are you?"
+echo "sita : Hi Ram, I'm doing well. How about you?"
+echo "ram : I'm good too sita. Long time no see!"
+echo "sita : Yes, it's been a while ram . Let's catch up soon."
