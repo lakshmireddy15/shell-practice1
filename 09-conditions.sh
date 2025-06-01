cab9b67b360300ce2811c007e1d4d4909bbab87e
lakshmi <nagalakshmireddy67@gmail..com>
2025-06-01 12:04:08 +0530

    message

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt ---- greather than
+# -lt----- less than
+# -eq ---- equal t
+# -ne ---- nt equal t 
+
+if [ $NUMBER -gt 10]
+then
+    echo  "Number is greather than 10 "
+else:
+    echo  "number is less than 10 "
---
+++ nodes.md
@@ -0,0 +1,14 @@
+git bash:
+DELL@DESKTOP-LNC7QP1 MINGW64 ~/Documents/DevOps/DevOps_Projects/repos
+
+git clone <url>
+git add .
+git commit -m "message"
+git push origin main
+
+
+ec2:
+git clone <url>
+git pull
+git
+sh <filename.sh>
